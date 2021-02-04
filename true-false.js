faa4d723b096b45c9a66efc9351a2c24307d9fff
Nazmul Ahsan <mnahsanofficial@gmail.com>
2021-02-05 01:02:52 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//false:
+//0
+//null
+//NaN
+// undefined
+//""
+
+//truth
+//' ','0',[]
+
+
+// const name="solaiman"
+let name = null;
+if(name){
+    console.log("Condition is true")
+}
+else{
+    console.log("condition is false")
+}
